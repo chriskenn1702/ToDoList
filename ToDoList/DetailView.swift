c0be6ed7fa055f3167e18252776b543b709c1449
Christopher Kennedy <kennednu@bc.edu>
2023-02-23 22:22:03 -0500

    5.2 - Lists

--- ToDoList/DetailView.swift
+++ ToDoList/DetailView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct DetailView: View {
     @Environment(\.dismiss) private var dismiss
+    
+    var passedValue: String
+    
     var body: some View {
         VStack{
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundColor(.orange)
-            Text("You Are a Swifty Legend!")
+            Text("You Are a Swifty Legend!\nAnd you passed over the value \(passedValue)")
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
             Spacer()
@@ -25,13 +28,12 @@ struct DetailView: View {
             .buttonStyle(.borderedProminent)
         }
         .padding()
-        .navigationBarBackButtonHidden()
     }
         
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(passedValue: "")
     }
 }
